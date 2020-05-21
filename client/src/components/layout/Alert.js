44cf4509e838dffb436aba6011266736b7cb3d9b
unknown <mcvilleanrapsody@gmail.com>
2020-05-21 21:07:19 +0800

    modify alert component

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -3,9 +3,11 @@ import { connect } from 'react-redux';
 
 const Alert = ({ alerts }) => {
     return (
-        alerts !== null && alerts.length > 0 && alerts.map(alert => (
+        <div className="container">
+            alerts !== null && alerts.length > 0 && alerts.map(alert => (
             <div key={alert.id} className={`alert alert-${alert.alertType}`}>{alert.msg}</div>
-        ))
+        ));
+        </div>
     );
 };
 
